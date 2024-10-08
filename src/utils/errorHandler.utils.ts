cc51c535d73249eeff6d94375d0ac3bc05cf596d
Abdulrasaq Taofeeq <abdulrasaqolarewaju88@gmail.com>
2024-10-08 23:53:16 +0100

    [fix]: deployment fixes

--- src/utils/errorHandler.utils.ts
+++ src/utils/errorHandler.utils.ts
@@ -1,26 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 
 const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-  let statusCode: any = undefined;
-  if (!error.statusCode) {
-    statusCode = res.statusCode === 200 ? 500 : error.statusCode
-  } else {
-    statusCode = error.statusCode
-  }
-  let errorMessage = error.message;
+  let statusCode = error.statusCode || (res.statusCode === 200 ? 500 : res.statusCode);
+  let errorMessage = error.message || 'Internal Server Error';
+
   if (Array.isArray(error.errors)) {
-    if (error.errors) {
-      const formattedErrors = error.errors.map((err: any) => `${err.message}`);
-      errorMessage += `: ${formattedErrors.join(', ')}`;
-    }
+    const formattedErrors = error.errors.map((err: any) => err.message);
+    errorMessage += `: ${formattedErrors.join(', ')}`;
   }
+
   return res.status(statusCode).json({
     status: false,
     error: errorMessage
-  })
-}
-
+  });
+};
 
 export {
   errorHandler
-}
+};
